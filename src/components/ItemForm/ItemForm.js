850b77b5c55ce35f2081c21e76155df207ee32d0
ben straumann <benstraum@gmail.com>
2020-06-28 15:14:42 -0500

    got rid of logger

--- src/components/ItemForm/ItemForm.js
+++ src/components/ItemForm/ItemForm.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import './ItemForm.css'
 import { connect } from 'react-redux';
-import axios from 'axios'
 class ItemForm extends Component {
-componentDidMount(){
-   this.props.dispatch({type:'GET_ITEMS'})
-}
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_ITEMS' })
+        console.log(this.props.state)
+    }
     state = {
         description: '',
-        items:''
+        items: ''
     }
     handleChange = (event, type) => {
         this.setState({
@@ -17,44 +17,47 @@ componentDidMount(){
         })
     }
     firePayload = () => {
-        this.props.dispatch({ type: 'FETCH_ITEMS', payload: this.state.description})
+        this.props.dispatch({ type: 'FETCH_ITEMS', payload: this.state.description })
         this.setState({
             description: '',
             url: ''
         })
     }
     addToShelf = () => {
-            this.firePayload()
+        this.firePayload()
     }
     render() {
         return (
             <div className='wrapper'>
                 <h3>Learn about dnd items</h3>
-                    {this.props.list ?
+                {this.props.list ?
                     <p>
-                    name:  {<br/>}
-                    {this.props.list.name} 
-                    {<br/>}
-                    description:  {<br/>}
-                    {this.props.list.desc}
-                    {<br/>}
-                    cost:  {<br/>}
-                    {JSON.stringify(this.props.list.cost)}
-                    {<br/>}
-                    weapon catagory:   {<br/>}
+                        name:  {<br />}
+                        {this.props.list.name}
+                        {<br />}
+                    description:  {<br />}
+                        {this.props.list.desc}
+                        {<br />}
+                    cost:  {<br />}
+                        {JSON.stringify(this.props.list.cost)}
+                        {<br />}
+                    weapon catagory:   {<br />}
                         {this.props.list.weapon_category}
-                    {<br/>}
-                    damage:  {<br/>}
-                    {JSON.stringify(this.props.list.damage)}
-                    {<br/>}
-                    armor class:   {<br/>}
-                    {JSON.stringify(this.props.list.armor_class)}
+                        {<br />}
+                    damage:  {<br />}
+                        {JSON.stringify(this.props.list.damage)}
+                        {<br />}
+                    armor class:   {<br />}
+                        {JSON.stringify(this.props.list.armor_class)}
+                        {<br />}
+                    Catagory:{<br />}
+                        {JSON.stringify(this.props.list.equipment_category)}
                     </p>
                     :
                     <h3>pick an item!</h3>
-                     }
-                    <select value={this.state.description} id='desc' placeholder='Item Description' onChange={(event) => this.handleChange(event, 'description')}>
-                   {this.props.items.map(item=>(<option key={item.index} value={item.index}>{item.name}</option>))}
+                }
+                <select value={this.state.description} id='desc' placeholder='Item Description' onChange={(event) => this.handleChange(event, 'description')}>
+                    {this.props.items.map(item => (<option key={item.index} value={item.index}>{item.name}</option>))}
                 </select>
                 <br />
                 <br />
@@ -65,8 +68,8 @@ componentDidMount(){
 }
 
 const mapStateToProps = state => ({
-    items:state.list.getItemReducer,
-    list:state.list.getListReducer
+    items: state.list.getItemReducer,
+    list: state.list.getListReducer
 });
 
 export default connect(mapStateToProps)(ItemForm);
